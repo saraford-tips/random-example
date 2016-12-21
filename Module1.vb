e97882a015e910d3b3210e5d1506c93251e1fd77
Sara Ford (Tips) <saraford-tips@users.noreply.github.com>
2016-12-20 18:03:20 -0800

    Added Visual Basic .NET sample
    Because VB... it's like reading an essay!

---
+++ Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Threading
+
+Module Module1
+    Public Sub Main()
+        Dim rand1 As New Random()
+        Dim rand2 As New Random()
+        Thread.Sleep(2000)
+        Dim rand3 As New Random()
+        ShowRandomNumbers(rand1)
+        ShowRandomNumbers(rand2)
+        ShowRandomNumbers(rand3)
+        Console.Read()
+    End Sub
+
+    Private Sub ShowRandomNumbers(rand As Random)
+        Console.WriteLine()
+        Dim values(4) As Byte
+        rand.NextBytes(values)
+        For Each value As Byte In values
+            Console.Write("{0, 5}", value)
+        Next
+        Console.WriteLine()
+    End Sub
+End Module
