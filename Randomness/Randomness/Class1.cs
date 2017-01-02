c830bc58e7479e26174422a0711d451b11437a2e
Sara Ford (Tips) <saraford-tips@users.noreply.github.com>
2017-01-02 14:43:30 -0800

    Revert "Fix #2"
    This reverts commit 3ae30dedf535d6bfacea0513e2acbe7a6c1f94bd.

---
+++ Randomness/Randomness/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomness
+{
+    class Class123
+    {
+        //public static void Main()
+        //{
+
+        //}
+    }
+}
